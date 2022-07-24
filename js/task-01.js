234e1808a934e1822cb0ef70a11315327c213186
Olena Nesteruk <pavlena81@gmail.com>
2022-07-24 13:25:31 +0300

    add firstEx

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+//     то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories,
+//     найдет и выведет в консоль текст заголовка элемента(тега < h2 >) 
+//     и количество элементов в категории(всех вложенных в него < li >)
+// const ulEl = document.querySelector('ul#categories');
+// console.log(ulEl);
+const ulEl = document.querySelectorAll(".item");
+const numberUlEl = ulEl.length;
+console.log('Number of categories: ${numberUlEl}');
+
+const nameForUlEl = ulEl.forEach((element) => {
+    console.log('Category: ${element.firstElementChild.textContent}');
+    console.log('Element: ${element.lastElementChild.Children.length}');
+})
