466e255d9ab0fc33bb0be474de36e4b53e73bf89
Olena Nesteruk <pavlena81@gmail.com>
2022-07-25 17:15:06 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,23 +5,28 @@
 // "Anonymous".
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+// =================================================
 
-inputEl.addEventListener("input", event => {
-  outputEl.textContent = event.currentTarget.value;
-});
- console.log(`Input: ${inputEl.value}`);
-function handleSubmit(event) {
-  event.preventDefault();
-       
+inputEl.addEventListener("input", onInputChange); 
+console.log(`Input: ${inputEl.value}`);
+ 
 
-  if (inputEl.value === "") {
-    outputEl.textContent = `${Anonymous}`;
+function onInputChange (event) {
+  // event.preventDefault();
+   if (inputEl.value === "") {
+    outputEl.textContent = `Anonymous`;
     return console.log("Please fill in  the fields!");
   }
+  outputEl.textContent = event.currentTarget.value; 
+ 
 }
 
-  console.log(`Input: ${inputEl.value}`);
-// //   event.currentTarget.reset();
-// 
+  console.log(inputEl);
+
+
+// ==========================================
+
+
+
 
 
