75ea81cdc12ee36a8ff9059ed5679a993b1f6d8a
Olena Nesteruk <pavlena81@gmail.com>
2022-07-27 18:13:32 +0300

    fixed task2

--- js/task-02.js
+++ js/task-02.js
@@ -16,35 +16,20 @@ const ingredients = [
   'Condiments',
 ];
 
+
 const arrIngredients = document.querySelector('#ingredients');
 console.log(arrIngredients);
 
-const itemPotatoes = document.createElement('li');
-itemPotatoes.textContent = 'Potatoes';
-itemPotatoes.classList.add('item');
-// console.log(itemPotatoes);
 
-const itemMushrooms = document.createElement('li');
-itemMushrooms.textContent = 'Mushrooms';
-itemMushrooms.classList.add('item');
+const ingredientsEl = ingredients.map(ingredient => {
+  const nameEl = document.createElement('li');
+  nameEl.textContent = ingredient;
+  nameEl.classList.add('item');
+  return nameEl;
+})
 
-const itemGarlic = document.createElement('li');
-itemGarlic.textContent = 'Garlic';
-itemGarlic.classList.add('item');
 
-const itemTomatos = document.createElement('li');
-itemTomatos.textContent = 'Tomatos';
-itemTomatos.classList.add('item');
-
-const itemHerbs = document.createElement('li');
-itemHerbs.textContent = 'Herbs';
-itemHerbs.classList.add('item');
-
-const itemCondiments = document.createElement('li');
-itemCondiments.textContent = 'Condiments';
-itemCondiments.classList.add('item');
-
-arrIngredients.append(itemPotatoes, itemMushrooms, itemGarlic, itemTomatos, itemHerbs, itemCondiments);
+arrIngredients.append(...ingredientsEl);
 
 
 
