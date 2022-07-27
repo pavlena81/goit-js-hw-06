826bb59e2a28cead3a1d3110fe82ea6734dc1dbf
Olena Nesteruk <pavlena81@gmail.com>
2022-07-28 00:54:12 +0300

    fixed task10

--- js/task-10.js
+++ js/task-10.js
@@ -28,35 +28,65 @@ function getRandomRgbColor() {
 
 const ref = {
   inputValue: document.querySelector('#controls>input'),
-  btnCreate: document.querySelector('button[data-create]'),
-  btnDestroy: document.querySelector('button[data-destroy]'),
-  boxArr: [],
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+  
   boxesEl: document.querySelector('#boxes'),
 }
-ref.btnCreate.addEventListener('click', createBoxes);
-
-function createBoxes (amount) {
-  amount = +ref.inputValue.value;
+  
+const createBoxes = (amount) => {
+  const boxArr = [];
+  amount = ref.inputValue.value;
 
 for (let i = 0; i < amount; i += 1) {
         const boxSize = 30;
-        let growBoxSize = boxSize + i * 10;
-        const boxItem = `<div style="width:${growBoxSize}px;
-    height:${growBoxSize}px; background-color:${getRandomHexColor()}" >
+        let growBoxSize = boxSize + i* 10;
+        const boxItem = `<div style='width:${growBoxSize}px;
+    height:${growBoxSize}px; background-color:${getRandomHexColor()}' >
     </div>`;
-        ref.boxArr.push(boxItem);
+  boxArr.push(boxItem);
+   ref.boxesEl.insertAdjacentHTML('afterbegin', boxArr.join(''));
+  // ref.boxesEl.append(...boxArr);
+}  
 }
-  return ref.boxArr;
-}
-
-ref.boxesEl.append(...ref.boxArr);
-
-
 
 const destroyBoxes = () => {
-  ref.boxesEl.innerHTML ='';
-  ref.inputValue.value ='';
-}
+  ref.boxesEl.innerHTML = '';
+  ref.inputValue.value = '';
+};
+
+ref.btnCreate.addEventListener('click', createBoxes);
 ref.btnDestroy.addEventListener('click', destroyBoxes);
 
 // ==================================================
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//   }
+//   function getRandomRgbColor() {
+//     const red = Math.round(Math.random(0, 1) * 255);
+//     const green = Math.round(Math.random(0, 1) * 255);
+//     const blue = Math.round(Math.random(0, 1) * 255);
+//     return `rgb(${red},${green},${blue})`;
+//   }
+//   const ref = {
+//     inputValue: document.querySelector('#controls>input'),
+//     btnCreate: document.querySelector('[data-create]'),
+//     btnDestroy: document.querySelector('[data-destroy]'),
+//     boxListHTML:  document.querySelector('#boxes'),
+//   };
+//   const createBoxes = (amount) => {
+//     const boxWrap = [];
+//   amount = ref.inputValue.value;
+//   for (let i = 0; i < amount; i += 1 ){
+//     const boxSize = 30;
+//     let growBox = boxSize + i * 10;
+//     const boxItem = `<div style ='width:${growBox}px ;height:${growBox}px; background-color:${getRandomRgbColor()}'></div>`
+//     boxWrap.push(boxItem);
+//   ref.boxListHTML.insertAdjacentHTML('afterbegin', boxWrap.join(''));
+//   }
+//   const destroyBoxes =() =>{
+//     ref.boxListHTML.innerHTML="";
+//     ref.inputValue.value = "";
+//   };
+//   ref.btnCreate.addEventListener('click', createBoxes );
+//   ref.btnDestroy.addEventListener('click', destroyBoxes);
