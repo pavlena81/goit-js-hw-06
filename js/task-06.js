c808288e9b9cc0b52f2925e33e16d082043b536f
Olena Nesteruk <pavlena81@gmail.com>
2022-07-27 18:48:08 +0300

    change  equality
    non-strict equality to strict

--- js/task-06.js
+++ js/task-06.js
@@ -14,9 +14,21 @@ const inputEl = document.querySelector("#validation-input");
 
 
 inputEl.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.length !== Number(inputEl.getAttribute(['data-length']))) {
-        inputEl.classList.add('invalid')
-    }
-        inputEl.classList.add('valid') 
+    if (event.currentTarget.value.length === Number(inputEl.getAttribute(['data-length']))) {
+        
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid'); 
+        
+}
+    });
+// =====================================================================        
+// inputEl.addEventListener("blur", (event) => {
+//     if (event.currentTarget.value.length !== Number(inputEl.getAttribute(['data-length']))) {
+//         inputEl.classList.add('invalid')
+//     }
+//         inputEl.classList.add('valid') 
     
-});
+// });
